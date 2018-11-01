5fb6523b8f9e9de6769ffa0410cb225e4296261e
amckean12 <amckean@me.com>
2018-11-01 18:33:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let recipeForm = document.getElementById("recipe-form-template").innerHTML;
+  let recipeFormFn = Handlebars.compile(recipeForm);
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function handleSubmit(){
+  let recipe = {
+    name: ,
+    description: ,
+    ingredients: [
+    ]
+  }
+}
